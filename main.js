f1157b7002dc3f839ff433d4f6fa71d259572ad4
feiben <feiben.dev@gmail.com>
2017-02-28 21:26:18 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+require.config({
+   paths:{
+       'jquery': './lib/jquery-3.1.1',
+       'flexible': './lib/flexible',
+       'backbone': './lib/backbone.min',
+       'underscore': 'underscore.min',
+       'text': './lib/text'
+   }
+});
+
+
+require(['jquery', 'flexible'], function($){
+
+});
